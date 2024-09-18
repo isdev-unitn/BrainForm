d43b62dd5f04405fcd51417815c7c7ed91356612
isdev-unitn <devis.zanoni@studenti.unitn.it>
2024-09-18 16:39:32 +0200

    fixed bug related to bci system dontdestroyonload and flash targets

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,24 +1,25 @@
 using Gtec.UnityInterface;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEditor;
 
 static class MenuConstants
 {
     public const int Level_menu = 0;
     public const string BciConfigurationTag = "BciConfiguration";
     public const string BciSystemTag = "BciSystem";
-    public const string AppTargetTempTag = "AppTargetTemp";
     public const string FlashControllerTag = "FlashController";
     public const string TrainingTargetTag = "TrainingTarget";
-    public const string OutlineControllerTag = "OutlineController";
+    public const string AppTargetTempTag = "AppTargetTemp";
 }
 
 public class MenuManager : MonoBehaviour
 {
     [SerializeField] private GameObject mainMenu;
     private GameObject trainingTarget;
+    private GameObject bciSystem;
     private GameObject bciControllerMenu;
-    private OutlineController outlineController;
     private bool mainMenuState;
     private bool bciControllerMenuState;
     private bool trainingTargetState;
@@ -37,19 +38,19 @@ public class MenuManager : MonoBehaviour
         DontDestroyOnLoad(GameObject.FindGameObjectWithTag(MenuConstants.BciSystemTag));
         DontDestroyOnLoad(GameObject.FindGameObjectWithTag(MenuConstants.AppTargetTempTag));
 
-        // problema attuale: la posizione del bci system va fixata ogni volta che apri il menu
-        bciControllerMenu = GameObject.FindGameObjectWithTag(MenuConstants.BciSystemTag).transform.GetChild(2).GetChild(5).gameObject;
+        bciSystem = GameObject.FindGameObjectWithTag(MenuConstants.BciSystemTag);
+        bciControllerMenu = bciSystem.transform.GetChild(2).GetChild(5).gameObject;
         trainingTarget = GameObject.FindGameObjectWithTag(MenuConstants.BciSystemTag).transform.GetChild(2).GetChild(4).gameObject;
-        outlineController = GameObject.FindGameObjectWithTag(MenuConstants.OutlineControllerTag).GetComponent<OutlineController>();
 
-        // add listeners for functions between bci system and menu scene
+        // add listener for function between bci system and menu scene
         bciControllerMenu.transform.GetChild(1).gameObject.GetComponent<Button>().onClick.AddListener(OpenCloseBCIMenu);
-        trainingTarget.GetComponent<FlashObject2D>().OnActivated.AddListener(delegate { outlineController.ActivateOutline(trainingTarget.transform.GetChild(2).GetChild(0).gameObject); });
-        trainingTarget.GetComponent<FlashObject2D>().OnDeactivated.AddListener(delegate { outlineController.DeactivateOutline(trainingTarget.transform.GetChild(2).GetChild(0).gameObject); });
     }
 
     void Start()
     {
+        bciSystem.transform.position = new Vector3(0, 0, 0); // reset bci system position to the center
+        setFlashControllerTargets(); // reset targets for flashcontroller
+
         mainMenuState = mainMenu.activeInHierarchy;
         bciControllerMenuState = bciControllerMenu.activeInHierarchy;
         trainingTargetState = trainingTarget.activeInHierarchy;
@@ -70,4 +71,25 @@ public class MenuManager : MonoBehaviour
     {
         Application.Quit();
     }
+
+
+    private void setFlashControllerTargets()
+    {
+        GameObject taskTargets = GameObject.FindGameObjectWithTag(MenuConstants.AppTargetTempTag);
+        // set number of classes, initially set at 10 to handle the biggest task in the game level 
+        bciSystem.transform.GetChild(3).gameObject.GetComponent<BCIConfiguration>().NumberOfClasses = 10;
+
+        // START preparing task targets list 
+        List<NeurofeedbackTarget> taskTargetElements = new List<NeurofeedbackTarget>();
+
+        for (int i = 0; i < taskTargets.transform.childCount; i++)
+        {
+            taskTargetElements.Add(taskTargets.transform.GetChild(i).gameObject.GetComponent<NeurofeedbackTarget>());
+        }
+        // END preparing task target list
+
+        // add new targets to the flash controller
+        bciSystem.transform.GetChild(1).gameObject.GetComponent<FlashController>().SetApplicationObjects(taskTargetElements);
+    }
+
 }
